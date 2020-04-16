0b9b937afe9415b10df51ce79cba8fecea3c0ac8
lgeshev <Lyubomir.Geshev@endava.com>
2020-04-16 18:56:08 +0300

    add kube provider with nginx web server

---
+++ kube.tf
@@ -0,0 +1,46 @@
+provider "kubernetes" {}
+
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name = "nginx-example"
+    labels = {
+      App = "nginx"
+    }
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.8"
+      name  = "example"
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_pod.nginx.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.nginx.load_balancer_ingress[0].ip
+}
+
+output "lb_hostname" {
+  value = kubernetes_service.nginx.load_balancer_ingress[0].hostname
+}

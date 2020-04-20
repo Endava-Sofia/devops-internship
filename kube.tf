3457a2109b53e6b935b5fdb4c7e5a14161ed2ef0
lgeshev <Lyubomir.Geshev@endava.com>
2020-04-20 22:15:34 +0300

    Destroy Kube recources

--- kube.tf
+++ kube.tf
@@ -1,46 +1,129 @@
-provider "kubernetes" {}
-
-resource "kubernetes_pod" "nginx" {
-  metadata {
-    name = "nginx-example"
-    labels = {
-      App = "nginx"
-    }
-  }
-
-  spec {
-    container {
-      image = "nginx:1.7.8"
-      name  = "example"
-
-      port {
-        container_port = 80
-      }
-    }
-  }
+provider "kubernetes" {
 }
 
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-example"
-  }
-  spec {
-    selector = {
-      App = kubernetes_pod.nginx.metadata[0].labels.App
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
+    # Error: timeout while waiting for state to become 'Running' (last state: 'Pending', timeout: 5m0s)
+    #    * nginx-example (Pod): FailedScheduling: no nodes available to schedule pods
 
-    type = "LoadBalancer"
-  }
-}
+    #   on kube.tf line 4, in resource "kubernetes_pod" "nginx":
+    #    4: resource "kubernetes_pod" "nginx" {
 
-output "lb_ip" {
-  value = kubernetes_service.nginx.load_balancer_ingress[0].ip
-}
+    # Error: timeout while waiting for state to become 'Running' (last state: 'Pending', timeout: 5m0s)
+    #  * nginx-example (Pod): FailedScheduling: no nodes available to schedule pods
+    #  * nginx-example (Pod): FailedScheduling: skip schedule deleting pod: default/nginx-example
 
-output "lb_hostname" {
-  value = kubernetes_service.nginx.load_balancer_ingress[0].hostname
-}
+    # on kube.tf line 10, in resource "kubernetes_pod" "nginx":
+    # 10: resource "kubernetes_pod" "nginx" {
+
+# resource "kubernetes_pod" "nginx" {
+#   metadata {
+#     name = "nginx-example"
+#     labels = {
+#       App = "nginx"
+#     }
+#   }
+
+#   spec {
+#     container {
+#       image = "nginx:1.7.8"
+#       name  = "example"
+
+#       port {
+#         container_port = 80
+#       }
+#     }
+#   }
+# }
+
+
+    # Error: Waiting for rollout to finish: 0 of 2 updated replicas are available...
+
+    #   on kube.tf line 24, in resource "kubernetes_deployment" "nginx":
+    #   24: resource "kubernetes_deployment" "nginx" {
+
+# resource "kubernetes_deployment" "nginx" {
+#   metadata {
+#     name = "scalable-nginx-example"
+#     labels = {
+#       App = "ScalableNginxExample"
+#     }
+#   }
+
+#   spec {
+#     replicas = 4
+#     selector {
+#       match_labels = {
+#         App = "ScalableNginxExample"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           App = "ScalableNginxExample"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "nginx:1.7.8"
+#           name  = "example"
+
+#           port {
+#             container_port = 80
+#           }
+
+#           resources {
+#             limits {
+#               cpu    = "0.5"
+#               memory = "512Mi"
+#             }
+#             requests {
+#               cpu    = "250m"
+#               memory = "50Mi"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+
+# resource "kubernetes_service" "nginx" {
+#   metadata {
+#     name = "nginx-example"
+#   }
+#   spec {
+#     selector = {
+#       App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+#     }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
+
+#     type = "LoadBalancer"
+#   }
+# }
+
+# resource "kubernetes_service" "nginx" {
+#   metadata {
+#     name = "nginx-example"
+#   }
+#   spec {
+#     selector = {
+#       App = kubernetes_pod.nginx.metadata[0].labels.App
+#     }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
+
+#     type = "LoadBalancer"
+#   }
+# }
+
+# output "lb_ip" {
+#   value = kubernetes_service.nginx.load_balancer_ingress[0].ip
+# }
+
+# output "lb_hostname" {
+#   value = kubernetes_service.nginx.load_balancer_ingress[0].hostname
+# }
--- main.tf
+++ main.tf
@@ -188,13 +188,13 @@ resource "aws_eks_cluster" "eks" {
   ]
 }
 
-output "endpoint" {
-  value = "${aws_eks_cluster.eks.endpoint}"
-}
+# output "endpoint" {
+#   value = "${aws_eks_cluster.eks.endpoint}"
+# }
 
-output "kubeconfig-certificate-authority-data" {
-  value = "${aws_eks_cluster.eks.certificate_authority.0.data}"
-}
+# output "kubeconfig-certificate-authority-data" {
+#   value = "${aws_eks_cluster.eks.certificate_authority.0.data}"
+# }
 
 
 resource "aws_iam_role" "eks_role" {
